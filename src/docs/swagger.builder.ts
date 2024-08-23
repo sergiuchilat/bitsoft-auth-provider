57fde4fc1d6f53784bac124e00a21356abd715b3
vicug <vicugaideic@gmail.com>
2024-08-23 16:43:05 +0300

    v1.0.14-beta

--- src/docs/swagger.builder.ts
+++ src/docs/swagger.builder.ts
@@ -5,7 +5,7 @@ const buildApiDocs = (app, ConfigEnv: ConfigInterface['docs']) => {
   const config = new DocumentBuilder()
     .setTitle(ConfigEnv.title)
     .setDescription(ConfigEnv.description)
-    .setVersion('v1.0.13-beta')
+    .setVersion('v1.0.14-beta')
     .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, ConfigEnv.authName)
     // .addGlobalParameters({
     //   name: 'x-api-key',
