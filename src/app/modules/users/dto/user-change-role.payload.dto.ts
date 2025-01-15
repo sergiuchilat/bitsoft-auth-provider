e21ea050470155f401d033e44d443b12efcd57a3
Daniel Maga <daniel.maga@icdsoft.ro>
2025-01-15 14:14:21 +0200

    Enum of roles - WRONG

--- src/app/modules/users/dto/user-change-role.payload.dto.ts
+++ src/app/modules/users/dto/user-change-role.payload.dto.ts
@@ -1,9 +1,9 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { UserRoleEnum } from '@/app/modules/users/enums/user-role.enum';
-import { IsEnum, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class UserChangeRolePayloadDto {
-  @ApiPropertyOptional({ example: UserRoleEnum.PUBLIC_USER, type: 'enum', enum: UserRoleEnum, title: 'Role' })
+  @ApiPropertyOptional({ example: UserRoleEnum.PUBLIC_USER, type: 'string', title: 'Role' })
   @IsString()
   role: UserRoleEnum;
 }
